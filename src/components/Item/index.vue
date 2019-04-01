34a2a48533a71b4104855a4dc513eea294252d97
kawa1228 <sxa.1105bo@gmail.com>
2019-04-02 00:46:10 +0900

    feat: コンポーネントを追加

---
+++ src/components/Item/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .item
+    h1.item__title this is item page
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ src/components/LoginHome/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .login-home
+    h1.login-home__title {{msg}}
+    button.login-home__button GoogleHomeでログイン
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface IData {
+  msg: string | null
+}
+export default Vue.extend({
+  data(): IData {
+    return {
+      msg: 'this is login home'
+    }
+  }
+})
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
