e2a8b1a0c07a9c1979778643163cca77612adbaf
kawa1228 <sxa.1105bo@gmail.com>
2019-04-03 00:10:16 +0900

    fix: タイポのためリネーム

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,7 +2,6 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 if (!firebase.apps.length) {
-  console.log(process.env.$APIKEY)
   firebase.initializeApp({
     apiKey: process.env.APIKEY,
     authDomain: process.env.AUTHDOMAIN,
